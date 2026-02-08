51fbf5233e9f9be3d885534b3bc3e7220f0251ac
paul <abayomipaulhenryhill@gmail.com>
2026-02-08 22:15:14 +0000

    Update Deployments.vue

--- frontend/src/views/Deployments.vue
+++ frontend/src/views/Deployments.vue
@@ -641,8 +641,8 @@
                      class="block w-full rounded-xl border-0 py-2.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-violet-500 sm:text-sm"
                    >
                    <p class="mt-1 text-xs text-gray-500">Internal port (e.g. 3000)</p>
+                </div>
              </div>
-
              <div>
                 <label class="block text-sm font-medium text-gray-700 mb-2">Dockerfile Path</label>
                 <input
